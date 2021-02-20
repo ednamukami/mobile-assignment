97f8e07ce10ec199ddfbd35130c4783ad82e6f99
student <ednnamukami@gmail.com>
2021-02-20 23:41:24 +0300

    first assignment

---
+++ src/mobile development.kt
@@ -0,0 +1,12 @@
+fun main (){
+    val student:String="Edna"
+    println(student)
+    var age:Byte=20
+    println(age)
+    val phonenumber:String="0713885055"
+    println(phonenumber)
+    var weight:Short=65
+    println(weight)
+    val kenyancitizen:Boolean=true
+    println(kenyancitizen)
+}
